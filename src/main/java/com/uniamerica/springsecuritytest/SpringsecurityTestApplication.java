e64a389b10497de75c20dcfc7eb647e9b0bed4dd
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-11-07 19:26:01 -0300

    Initial commit

---
+++ src/main/java/com/uniamerica/springsecuritytest/SpringsecurityTestApplication.java
@@ -0,0 +1,13 @@
+package com.uniamerica.springsecuritytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniamerica/springsecuritytest/SpringsecurityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniamerica.springsecuritytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
